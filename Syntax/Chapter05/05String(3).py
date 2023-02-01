42d2a3a1442ae0a46a5f11f905fa52b7e3703a02
Jeong Yeon Jae <jyj010818@naver.com>
2023-02-02 00:03:16 +0900

    Basic Syntax

---
+++ Syntax/Chapter05/05String(3).py
@@ -0,0 +1,20 @@
+si_1 = "용인"
+gu_1 = "기흥"
+address_1 = f"{si_1}시 {gu_1}구"
+
+si_2 = "서울"
+gu_2 = "종로"
+address_2 = f"{si_2}시 {gu_2}구"
+
+# 서울시 종로구
+# 용인시 기흥구
+# (시의 이름)시 + (구의 이름)구
+
+print(f"{si_1}시 {gu_1}구")
+print(f"{si_2}시 {gu_2}구")
+
+print(address_1)
+print(address_2)
+
+# f string 사용 방법
+# 변수명 = f"{문자열이 담겨있는 변수}"
