11604c0a641deb7534e6232f03c2b17f1abcb6c7
Jeong Yeon Jae <jyj010818@naver.com>
2023-02-07 00:04:23 +0900

    Python

---
+++ Syntax/Chapter05/09Boolean.py
@@ -0,0 +1,17 @@
+# 논리형(Bool, Boolean)
+# 대문자로 시작하는 키워드
+
+# True, False -> 파이썬에서는 앞 글자를 대문자로 씀
+
+print(3 > 2) # True
+
+# == 연산자는 좌변과 우변의 값이 같은지를 비교함.
+# 반환값은 Bool형
+
+print(3 == 2) # False
+print(3 == 3.0)
+
+# is와 ==는 다름.
+# is는 변수의 형까지 판단함
+
+print(3 is 3.0) # False
---
+++ Syntax/Chapter05/10Prompt.py
@@ -0,0 +1,4 @@
+# 명령 프롬프트 (prompt)
+
+value = input("Input the Value : ")
+print(value)
