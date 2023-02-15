5d1d698cfb98578480bd57a4d054e8cf2f16014e
Jeong Yeon Jae <jyj010818@naver.com>
2023-02-15 21:15:28 +0900

    Python

---
+++ Syntax/Chapter07/03List(3).py
@@ -0,0 +1,22 @@
+# List Sort
+
+colors = ['blue', 'red', 'gray', 'black', 'yellow', 'orange']
+
+# 정렬 1
+# 순방향 정렬
+# colors.sort()
+# print(colors)
+
+# 역방향 정렬
+# colors.sort(reverse=True)
+# print(colors)
+
+# 정렬 2
+# 일시적으로 정렬했다가 원 상태로 돌아옴
+print(sorted(colors))
+
+# 길이 - 요소의 갯수
+print(len(colors))
+
+
+
